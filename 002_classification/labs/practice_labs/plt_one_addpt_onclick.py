a6e9f62efc03eb3297af6b4743275bc13d181ebd
abhishek luthra <er.abhishek.luthra@gmail.com>
2025-03-25 17:47:32 +0530

    Updated requirements.txt to include ipympl version 0.9.7. Modified Jupyter notebooks to update Python version to 3.10.8 in C1_W3_Lab01 and C1_W3_Lab02, and changed version to 3.9.6 in C1_W3_Lab09. Removed unused resize_sq method from plt_one_addpt_onclick.py and added inline matplotlib support in C1_W3_Lab01.

--- 002_classification/labs/practice_labs/plt_one_addpt_onclick.py
+++ 002_classification/labs/practice_labs/plt_one_addpt_onclick.py
@@ -63,7 +63,6 @@ class plt_one_addpt_onclick:
             self.bcalc.on_clicked(self.calc_linear)
         self.bthresh = CheckButtons(axthresh, ('Toggle 0.5 threshold (after regression)',))
         self.bthresh.on_clicked(self.thresh)
-        self.resize_sq(self.bthresh)
 
  #   @output.capture()  # debug
     def add_data(self, event):
@@ -167,20 +166,3 @@ class plt_one_addpt_onclick:
             artist.remove()
         self.fig.canvas.draw()
 
-    def resize_sq(self, bcid):
-        """ resizes the check box """
-        #future reference
-        #print(f"width  : {bcid.rectangles[0].get_width()}")
-        #print(f"height : {bcid.rectangles[0].get_height()}")
-        #print(f"xy     : {bcid.rectangles[0].get_xy()}")
-        #print(f"bb     : {bcid.rectangles[0].get_bbox()}")
-        #print(f"points : {bcid.rectangles[0].get_bbox().get_points()}")  #[[xmin,ymin],[xmax,ymax]]
-
-        h = bcid.rectangles[0].get_height()
-        bcid.rectangles[0].set_height(3*h)
-
-        ymax = bcid.rectangles[0].get_bbox().y1
-        ymin = bcid.rectangles[0].get_bbox().y0
-
-        bcid.lines[0][0].set_ydata([ymax,ymin])
-        bcid.lines[0][1].set_ydata([ymin,ymax])
